b99a88da2643cfff0ccdd68543646212dea94497
hitsugaya11 <yerdos_ilyassov@epam.com>
2018-05-12 15:59:53 +0300

    first variant

--- src/test/java/com/epam/GameEngineBasisConfigurationsTest.java
+++ src/test/java/com/epam/GameEngineBasisConfigurationsTest.java
@@ -1,8 +1,10 @@
 package com.epam;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class GameEngineBasisConfigurationsTest {
+    GameEngine gameEngine=new GameEngineImpl1();
 
     @Test
     void standardAliveSectorTest2() {
@@ -15,7 +17,7 @@ class GameEngineBasisConfigurationsTest {
         };
         int numberIterations = 1;
 
-        // TODO execute
+
 
         boolean[][] expectedState = {
                 {false, false, false, false, false},
@@ -24,6 +26,155 @@ class GameEngineBasisConfigurationsTest {
                 {false, false, false, false, false},
                 {false, false, false, false, false}
         };
-        // TODO assertions
+        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
     }
+
+
+    @Test
+    void standardAliveSectorTest3() {
+        boolean[][] initialState = {
+                {false, false, false, false, false},
+                {false, true,  false, false, false},
+                {false, false, true, true, false},
+                {false, true, true,  false, false},
+                {false, false, false, false, false}
+        };
+        int numberIterations = 1;
+
+
+
+        boolean[][] expectedState = {
+                {false, false, false, false, false},
+                {false, false, true, false, false},
+                {false, false, false,  true, false},
+                {false, true, true, true, false},
+                {false, false, false, false, false}
+        };
+        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+    }
+
+
+
+
+    @Test
+    void standardAliveSectorTest4() {
+        boolean[][] initialState = {
+                {false, false, false, false, false},
+                {false, true,  false, false, false},
+                {false, false, false, true, false},
+                {false, false, true,  false, false},
+                {false, false, false, false, false}
+        };
+        int numberIterations = 2;
+
+
+
+        boolean[][] expectedState = {
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false,  false, false},
+                {false, false, false, false, false},
+                {false, false, false, false, false}
+        };
+        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+    }
+
+
+//    @Test
+//    void standardAliveSectorTest5() {
+//        boolean[][] initialState = {
+//                {false, false, false, false, false},
+//                {false, false, true, false, false},
+//                {false, false, false, true, false},
+//                {false, true, true, true, false},
+//                {false, false, false, false, false}
+//        };
+//        int numberIterations = 6;
+//
+//
+//
+//
+//        boolean[][] expectedState = {
+//                {false, false, false, true, true},
+//                {false, false, false, false, false},
+//                {false, false, false, false, false},
+//                {false, false, false, false, true},
+//                {false, false, true, false, true}
+//        };
+//        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+//    }
+@Test
+void standardAliveSectorTest5() {
+    boolean[][] initialState = {
+            {false, false, false, true, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, true, false, true},
+            {false, false, false, true, true}
+    };
+    int numberIterations = 1;
+
+
+
+
+    boolean[][] expectedState = {
+            {false, false, false, true, true},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, true},
+            {false, false, true, false, true}
+    };
+    Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+}
+
+    @Test
+    void standardAliveSectorTest6() {
+        boolean[][] initialState = {
+                {false, false, false, false, false},
+                {false, false, true, false, false},
+                {false, false, false, true, false},
+                {false, true, true, true, false},
+                {false, false, false, false, false}
+        };
+        int numberIterations = 2;
+
+
+
+
+        boolean[][] expectedState = {
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false, true, false},
+                {false, true, false, true, false},
+                {false, false, true, true, false}
+        };
+        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+    }
+
+    @Test
+    void standardAliveSectorTest7() {
+        boolean[][] initialState = {
+                {false, false, false, false, false},
+                {false, false, true, false, false},
+                {false, false, false, true, false},
+                {false, true, true, true, false},
+                {false, false, false, false, false}
+        };
+        int numberIterations = 6;
+
+
+
+
+        boolean[][] expectedState = {
+                {false, false, false, true, true},
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false, false, true},
+                {false, false, true, false, true}
+        };
+        Assertions.assertArrayEquals(expectedState,gameEngine.compute(initialState,numberIterations));
+    }
+
+
+
 }
